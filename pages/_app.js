2c09263241df0035b17fcd200fe44ab792032634
Abhinavkar <karabhinav24@gmail.com>
2023-06-26 14:10:06 +0530

    added-todolist

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
-import '@/styles/globals.css'
+// pages/_app.js
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// 1. Import `createTheme`
+import { createTheme, NextUIProvider } from "@nextui-org/react"
+
+// 2. Call `createTheme` and pass your custom theme values
+const theme = createTheme({
+  type: "dark", // it could be "light" or "dark"
+  theme: {
+    colors: {
+      primary: '#176B87',
+      secondary: '#F9CB80',
+      error: '#FCC5D8',
+    },
+  }
+})
+
+
+// 3. Pass the new `theme`` to the `NextUIProvider`
+function App({ Component, pageProps }) {
+  return (
+    <NextUIProvider theme={theme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
 }
+
+export default App;
